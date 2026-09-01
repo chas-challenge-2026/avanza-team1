49cf3a3790671336e0a08a8ed4a3098226ce930e
bjorneboman <1125969+bjorneboman@users.noreply.github.com>
2026-09-01 09:45:28 +0200

    feature[frontend]: Add layout styling to Navbar.css + add semantic page structure to App.tsx (header, main, footer)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,28 @@
 // import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
-  <BrowserRouter>
-    <Navbar/>
-      <Routes>
-        <Route path="/" element={null} />
-        <Route path="/holdings" element={null} />
-        <Route path="/login" element={null} />
-        <Route path="/alerts" element={null} />
-      </Routes>
-  </BrowserRouter>
+    <>
+      <header>
+        <Navbar/>
+          <Routes>
+            <Route path="/" element={null} />
+            <Route path="/holdings" element={null} />
+            <Route path="/login" element={null} />
+            <Route path="/alerts" element={null} />
+          </Routes>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </>
   )
 }
 
