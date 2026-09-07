5ee2adfb9769eeb0cd68804d1e5bc5ca37dc40ec
bjorneboman <1125969+bjorneboman@users.noreply.github.com>
2026-09-07 14:37:51 +0200

    Chore[frontend] Clean up main.tsx and remove duplicate lines in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 // import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 import Navbar from "./components/Navbar";
 import Portfolio from './pages/Portfolio';
 import Holdings from './pages/Holdings';
@@ -15,10 +14,10 @@ function App() {
       </header>
       <main className="app-content">
         <Routes>
-        <Route path="/" element={<Portfolio />} />
-        <Route path="/holdings" element={<Holdings />} />
-        <Route path="/alerts" element={<Alerts />} />
-        <Route path="/login" element={null} />
+          <Route path="/" element={<Portfolio />} />
+          <Route path="/holdings" element={<Holdings />} />
+          <Route path="/alerts" element={<Alerts />} />
+          <Route path="/login" element={null} />
         </Routes>
       </main>
     </>
