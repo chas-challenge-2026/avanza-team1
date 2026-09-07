797a1d1dea815d247debac098fa50581bbed7bf5
Henrik Westerlund <henrik.w93@gmail.com>
2026-09-07 15:26:43 +0200

    #17 [Native] Finished basic FX module. Still needs polish but basic functionality is there.

--- native/fx/fx.cpp
+++ native/fx/fx.cpp
@@ -28,35 +28,45 @@ size_t write_data(char *buffer, size_t size, size_t nmemb, void *user_data)
     return real_size;
 }
 
-void fx_convert(FX_Data *fx_data, const char *curr1, const char *curr2)
+FX_Code fx_convert(FX_Data *fx_data, const char *curr1, const char *curr2)
 {
     if (fx_data == NULL)
     {
-        std::cout << "Error: Invalid FX_Data struct\r\n";
-        return;
+        return FX_ERR_NULLPTR;
     }
 
-    // Expect currencies to be 3 letters (examples: SEK, GBP, EUR, USD)
+    /*  Expect currencies to be 3 letters (examples: SEK, GBP, EUR, USD) */
     if ((strlen(curr1) != 3) || (strlen(curr2) != 3))
     {
-        std::cout << "Error: Invalid currency\r\n";
-        return;
+        return FX_ERR_INVALID_CURRENCY;
     }
 
     char url[URL_LEN];
 
     snprintf(url, URL_LEN, "https://api.riksbank.se/swea/v1/Observations/Latest/%s%spmi", curr1, curr2);
 
-    // response.string gets allocated on the heap in fx_curl, remember to free after parsing
+    /*  response.string gets allocated on the heap in fx_curl, remember to free after parsing */
     Response response;
-    fx_curl(url, &response);
+    FX_Code result = fx_curl(url, &response);
+    if (result != FX_OK)
+    {
+        free(response.string);
+        return result;
+    }
 
-    fx_parse_string(fx_data, response.string);
+    result = fx_parse_string(fx_data, response.string);
+    if (result != FX_OK)
+    {
+        free(response.string);
+        return result;
+    }
 
     free(response.string);
+
+    return FX_OK;
 }
 
-void fx_curl(const char *url, Response *response)
+FX_Code fx_curl(const char *url, Response *response)
 {
     // std::cout << "url: " << url << "\r\n";
 
@@ -64,9 +74,7 @@ void fx_curl(const char *url, Response *response)
     CURLcode result;
     if (handle == NULL)
     {
-        std::cout << "HTTP request failed\r\n";
-        // return NULL;
-        return;
+        return FX_ERR_CURL;
     }
 
     response->string = (char*)malloc(1);
@@ -80,50 +88,45 @@ void fx_curl(const char *url, Response *response)
     if (result != CURLE_OK)
     {
         std::cout << "Error: " << curl_easy_strerror(result) << "\r\n";
-        // return NULL;
-        return;
+        return FX_ERR_CURL;
     }
 
     curl_easy_cleanup(handle);
-
-    // int len = strlen(response->string);
-
-    // char *full_string = (char*)malloc(len + 1);
-    // strncpy(full_string, response->string, len);
-
-    // std::cout << "Response: " << response->string << "\r\n";
-    // free(response->string);
+    
+    return FX_OK;
 }
 
-void fx_parse_string(FX_Data *fx_data, const char *buffer)
+FX_Code fx_parse_string(FX_Data *fx_data, const char *buffer)
 {
-    // const char *test_string = "{\"date\":\"2026-09-01\",\"value\":11.1145}";
-
     json_error_t json_error;
     json_t *json = json_loads(buffer, 0, &json_error);
     if (!json)
     {
         std::cout << "Error on line " << json_error.line << ": " << json_error.text << "\r\n";
+        return FX_ERR_JSON;
     }
 
     json_t *json_date = json_object_get(json, "date");
-    if (json_is_string(json_date))
+    if (!json_is_string(json_date))
     {
-        const char *str = json_string_value(json_date);
-
-        std::cout << "str: " << str << "\r\n";
-        snprintf(fx_data->date, sizeof(char) * 11, "%s", str);
+        json_decref(json);
+        return FX_ERR_JSON;
     }
+    
+    const char *str = json_string_value(json_date);
+    snprintf(fx_data->date, sizeof(char) * 11, "%s", str);
 
     json_t *json_value = json_object_get(json, "value");
-    if (json_is_real(json_value))
+    if (!json_is_real(json_value))
     {
-        double value = json_real_value(json_value);
-
-        std::cout << "value: " << value << "\r\n";
-        fx_data->rate = value;
+        json_decref(json);
+        return FX_ERR_JSON;
     }
-   
+
+    double value = json_real_value(json_value);
+    fx_data->rate = value;   
 
     json_decref(json);
+
+    return FX_OK;
 }
