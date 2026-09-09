b969bfe05557475c99f4b72476b707ac44904c5f
bjorneboman <1125969+bjorneboman@users.noreply.github.com>
2026-09-09 12:18:03 +0200

    Styling(frontend) Rename page top component from Navbar to Topbar and update all imports (#45) + Clear out obsolete comments in index.css amd App.css + Replace brand text with logo (#45) + Update Topbar to follow naming conventions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Topbar";
 import Portfolio from './pages/Portfolio';
 import Holdings from './pages/Holdings';
 import Alerts from './pages/Alerts';
--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,22 +1,22 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import styles from "./Navbar.module.css";
+import styles from "./Topbar.module.css";
 import { mockPortfolio } from "../types/portfolio";
 
-function Navbar() {
+function Topbar() {
   const navigate = useNavigate();
 
   return (
-    <header className={styles.navbar}>
-        <div className={styles["navbar-brand"]}>
-          <h1>Avanza</h1>
+    <header className={styles.topbar}>
+        <div className={styles.brand}>
+          <img src="src/assets/logo.webp" alt="Logo" className={styles.logo} />
         </div>
         <nav>
           <NavLink to="/" end className={({ isActive }) => isActive ? styles.active : ""}>Portfolio</NavLink>
           <NavLink to="/holdings" className={({ isActive }) => isActive ? styles.active : ""}>Innehav</NavLink>
           <NavLink to="/alerts" className={({ isActive }) => isActive ? styles.active : ""}>Notiser</NavLink>
         </nav>
-        <div className="navbar-user">
-          <span className="navbar-user-name">{mockPortfolio.userName}</span>
+        <div className={styles.user}>
+          <span className={styles["user-name"]}>{mockPortfolio.userName}</span>
           <button className={styles["logout-button"]} onClick={() => navigate("/login")}>
             Logga ut
           </button>
@@ -25,4 +25,4 @@ function Navbar() {
   );
 }
 
-export default Navbar;
+export default Topbar;
